8437040d82c221c97de266a63f58919f3c200cd1
Kenny <kzhao7301@gmail.com>
2018-12-03 15:25:25 -0500

    logged when book service returns an empty list

--- server/src/main/java/com/example/zhao/kenny/demo/BookController.java
+++ server/src/main/java/com/example/zhao/kenny/demo/BookController.java
@@ -3,6 +3,7 @@ package com.example.zhao.kenny.demo;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,12 +27,14 @@ public class BookController {
 
     @Autowired
     public BookService bookService;
+    public final static Logger logger = Logger.getLogger(BookService.class);
 
     @CrossOrigin
     @RequestMapping(value = "/search", method = RequestMethod.GET)
     public ResponseEntity<List<Book>> searchBooks(@RequestParam(value="q") String query){
         List<Book> bookResults = bookService.getBooks(query);
         if(bookResults.isEmpty()){
+            logger.info(String.format("Query %s returned an empty list.", query));
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<List<Book>>(bookResults, HttpStatus.OK);
